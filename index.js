ff4076a2d47f87753cb32c5587ca635aa56ee2d2
loyd010 <94154952+loyd010@users.noreply.github.com>
2022-03-30 19:49:09 -0500

    Added JSON object with movie data

--- index.js
+++ index.js
@@ -5,6 +5,59 @@ const app = express();
 
 app.use(morgan('common'));
 
+let topMovies = [
+  {
+    title: 'The Martian',
+    director: 'Ridley Scott',
+    genre: 'adventure, science fiction'
+  },
+  {
+    title: 'Inception',
+    director: 'Christopher Nolan',
+    genre: 'action, science fiction'
+  },
+  {
+    title: 'The Departed',
+    director: 'Martin Scorsese',
+    genre: 'crime, drama, thriller'
+  },
+  {
+    title: 'Blood Diamond',
+    director: 'Edward Zwick',
+    genre: 'drama, thriller'
+  },
+  {
+    title: 'The Goonies',
+    director: 'Richard Donner',
+    genre: 'adventure, comedy'
+  },
+  {
+    title: 'The Shawshank Redemption',
+    director: 'Frank Darabont',
+    genre: 'drama'
+  },
+  {
+    title: 'The Dark Knight',
+    director: 'Christopher Nolan',
+    genre: 'action, crime'
+  },
+  {
+    title: 'Saving Private Ryan',
+    director: 'Steven Spielberg',
+    genre: 'drama, war'
+  },
+  {
+    title: 'Interstellar',
+    director: 'Christopher Nolan',
+    genre: 'drama, science fiction'
+  },
+  {
+    title: 'Snatch',
+    director: 'Guy Ritchie',
+    genre: 'comedy, crime'
+  }
+];
+
 app.get('/', (req, res) => {
   res.send('Welcome to Tara\'s Top Ten Movies!');
 });
