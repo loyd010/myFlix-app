ec54b1dd49459fa7dd75f0fce08ca02174285ebe
loyd010 <94154952+loyd010@users.noreply.github.com>
2022-05-17 21:19:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -230,6 +230,6 @@ app.get('/movies', (req, res) => {
 app.use(express.static('public'));
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port ' + port);
 });
