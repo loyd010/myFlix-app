7bfb1f7c85d52b29a742673c887a44e65d332c29
loyd010 <94154952+loyd010@users.noreply.github.com>
2022-05-17 19:55:57 -0500

    Removed unused code

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ app.post('/users', [
         Email: req.body.Email,
         Birthday: req.body.Birthday
       })
-      .then((user) => {res.status(202).json(user) })
+      .then((user) => {res.status(201).json(user) })
       .catch((error) => {
         console.error(error);
         res.status(500).send('Error: ' + error);
@@ -227,67 +227,7 @@ app.get('/movies', (req, res) => {
   res.status(200).json(movies);
 });
 
-/*Start of code for Exercise 2.5
-app.get('/movies/:title', (req, res) => {
-  const { title } = req.params;
-  const movie = movies.find( movie => movie.title === title );
-
-  if (movie) {
-    res.status(200).json(movie);
-  }  else {
-    res.status(400).send('This movie not found')
-  }
-});
-
-app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find( movie => movie.genre.Name === genreName ).genre;
-
-  if (genre) {
-    res.status(200).json(genre);
-  }  else {
-    res.status(400).send('This genre not found')
-  }
-});
-
-app.get('/movies/director/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find( movie => movie.director.Name === directorName ).director;
-
-  if (director) {
-    res.status(200).json(director);
-  }  else {
-    res.status(400).send('This director not found')
-  }
-});
-
-app.post('/users', (req, res) => {
-  res.send('Successful creation of a new user profile')
-});
-
-app.put('/users/:userName', (req, res) => {
-  res.send('Username has been successfully updated')
-});
-
-app.post('/users/userName/:Favorites', (req, res) => {
-  res.send('Your movie has been added to your Favorites')
-});
-
-app.delete('/users/userName/:Removals', (req, res) => {
-  res.send('Your movie has been removed from your Favorites')
-});
-
-app.put('/users/userName/:Registration', (req, res) => {
-  res.send('User email has successfully been removed')
-});
-*/
-
 app.use(express.static('public'));
-
-/*app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});*/
 
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
